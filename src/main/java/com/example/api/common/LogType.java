0ee2da45a6e0f05e0d8045df327229cc3e49c8a1
Andrew Abad <dev.arabad@gmail.com>
2020-07-17 10:03:24 +0800

    [add] [20200717] [ara] - Added logging type

---
+++ src/main/java/com/example/api/common/LogType.java
@@ -0,0 +1,10 @@
+package com.example.api.common;
+
+public enum LogType
+{
+  // Using the toString() of the Object
+  STRING
+
+  // Converts the Object to a JsonString using Jackson ObjectMapper
+  , JSON_STRING
+}
